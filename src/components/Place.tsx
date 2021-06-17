f2e99e7d886c4cb08336d6c680802df25d49ca1a
Seb <sebwiggins@me.com>
2021-06-17 13:52:24 +0100

    Place Component

--- src/components/Place.tsx
+++ src/components/Place.tsx
@@ -6,3 +6,17 @@ interface PlaceProps {
     googleLocation: URL;
     text: string;
   }
+
+  function Place(props: PlaceProps): JSX.Element {
+      return (
+          <section>
+              <h2>{props.title}</h2>
+              <h3>
+                 <p>{props.placeName} {props.country} ({props.googleLocation})</p> 
+              </h3>
+              <p>{props.text}</p>
+          </section>
+      );
+  }
+
+  export default Place
