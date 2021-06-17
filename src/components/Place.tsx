0ab7f1aa4a9ff57dee954f5f3dc9e23309b05934
Seb <sebwiggins@me.com>
2021-06-17 13:59:52 +0100

    Updated Place Component

--- src/components/Place.tsx
+++ src/components/Place.tsx
@@ -1,22 +1,24 @@
 interface PlaceProps {
-    title: string;
-    placeName: string[];
-    country: string;
-    image: URL;
-    googleLocation: URL;
-    text: string;
-  }
+  title: string;
+  placeName: string;
+  country: string;
+  image: URL;
+  googleLocation: URL;
+  text: string;
+}
 
-  function Place(props: PlaceProps): JSX.Element {
-      return (
-          <section>
-              <h2>{props.title}</h2>
-              <h3>
-                 <p>{props.placeName} {props.country} ({props.googleLocation})</p> 
-              </h3>
-              <p>{props.text}</p>
-          </section>
-      );
-  }
+function Place(props: PlaceProps): JSX.Element {
+  return (
+    <section>
+      <h2>{props.title}</h2>
+      <h3>
+        <p>
+          {props.placeName} {props.country} ({props.googleLocation})
+        </p>
+      </h3>
+      <p>{props.text}</p>
+    </section>
+  );
+}
 
-  export default Place
+export default Place;
