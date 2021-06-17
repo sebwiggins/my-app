39cea41fdbace5d29bea425cb13a56f7a5283916
Seb <sebwiggins@me.com>
2021-06-17 14:45:15 +0100

    imports for App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-// import React from "react";
-// import logo from "./logo.svg";
+import React from "react";
+import logo from "./logo.svg";
 import "./App.css";
 import MainContent from "./components/MainContent"; 
 import PageHeader from "./components/PageHeader";
