105336b9e7206a4afdd6157a7d444c5fd9f9bda6
Seb <sebwiggins@me.com>
2021-06-18 16:07:28 +0100

    completed app

--- src/components/Place.tsx
+++ src/components/Place.tsx
@@ -10,7 +10,7 @@ interface PlaceProps {
 function Place(props: PlaceProps): JSX.Element {
   return (
     <section>
-         <img src='https://www.google.co.uk/url?sa=i&url=https%3A%2F%2Fwww.walesonline.co.uk%2Flifestyle%2Fwelsh-homes%2Fcheapest-places-buy-home-cardiff-19006526&psig=AOvVaw1CHO4kYeUUHasq4bkpIOjl&ust=1624093300257000&source=images&cd=vfe&ved=0CAoQjRxqFwoTCODC8pjpoPECFQAAAAAdAAAAABAD' alt= "Cardiff city centre" />
+         <img src={props.image} alt= "Cardiff City Centre" />
       <h2>{props.title}</h2>
       <h3>
         <p>
