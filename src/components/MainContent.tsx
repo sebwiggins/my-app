bc9e8e9fbed16d50203ac12b31800ea51f8f31de
Seb <sebwiggins@me.com>
2021-06-18 10:10:41 +0100

    link to string

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -8,24 +8,28 @@ function MainContent(): JSX.Element {
             placeName={"Cardiff"}
             country={"Wales"}
             text={"The capital city of Wales, has a castle in the centre of the city"}
+            googleLocation={"https://www.google.com/maps/place/Cardiff/@51.5254472,-3.2294204,11.2z/data=!4m5!3m4!1s0x486e02d434ec53f5:0x143406db6586670e!8m2!3d51.481581!4d-3.17909"}
             />
             <Place 
             title={"The islands"}
             placeName={"Lhaviyani Atoll"}
             country={"Maldives"}
             text={"The clearest water you'll ever see"}
+            googleLocation={"https://www.google.com/maps/place/Cardiff/@51.5254472,-3.2294204,11.2z/data=!4m5!3m4!1s0x486e02d434ec53f5:0x143406db6586670e!8m2!3d51.481581!4d-3.17909"}
             />
              <Place 
             title={"The Big Five"}
             placeName={"Kruger National Park"}
             country={"South Africa"}
             text={"Never thought i'd get bored of seeing elephants, but I did"}
+            googleLocation={"https://www.google.com/maps/place/Cardiff/@51.5254472,-3.2294204,11.2z/data=!4m5!3m4!1s0x486e02d434ec53f5:0x143406db6586670e!8m2!3d51.481581!4d-3.17909"}
             />
               <Place 
             title={"Fresh Powder"}
             placeName={"Val D'isere"}
             country={"France"}
             text={"Amazing scenery, skiing conditions and food"}
+            googleLocation={"https://www.google.com/maps/place/Cardiff/@51.5254472,-3.2294204,11.2z/data=!4m5!3m4!1s0x486e02d434ec53f5:0x143406db6586670e!8m2!3d51.481581!4d-3.17909"}
             />
         </main>
     );
